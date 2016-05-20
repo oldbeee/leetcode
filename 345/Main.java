6619ac082bd477fd16cd9921a6998a61f5fe5697
wangshenbi <biwangshen_sx@qiyi.com>
2016-05-20 11:10:39 +0800

    push the wrong profile

---
+++ 345/Main.java
@@ -0,0 +1,13 @@
+package com.qiyi.wangshenbi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "urUtklaopie";
+		Solution solution=new Solution();
+		String result = solution.reverseVowels(str);
+		System.out.println(result);
+	}
+
+}
--- 345/Solution.java
+++ 345/Solution.java
@@ -1,27 +1,36 @@
 package com.qiyi.wangshenbi;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 public class Solution {
-	public int[] intersection(int[] a,int[] b){
-		HashSet<Integer> a1 = new HashSet<Integer>();
-		for(int i=0;i<a.length;i++)
-			a1.add(a[i]);
-		List<Integer> tmp = new ArrayList<Integer>();
-		for(int i=0;i<b.length;i++){
-			if(a1.contains(b[i])){
-				tmp.add(b[i]);
-				a1.remove(b[i]);
-				//System.out.println(b.length);
+	public String reverseVowels(String s){
+		char[] a;
+		a=s.toCharArray();
+		int i=0;
+		int j=a.length-1;
+		while(i<j){
+			if(isVowels(a[i])&&isVowels(a[j])){
+				char tmp;
+				tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+				i++;
+				j--;
+			}else if(isVowels(a[i])){
+				j--;
+			}else{
+				i++;
 			}
 		}
-		int result[] = new int[tmp.size()];
-		for(int i=0;i<result.length;i++){
-			result[i] = tmp.get(i);
-		}
+		String result = String.valueOf(a);
 		return result;
 	}
-
+	private boolean isVowels(char word){
+		if (word=='a'||word=='e'||word=='i'||
+				word=='o'||word=='u'||word=='A'||
+				word=='E'||word=='I'||word=='O'||
+				word=='U'){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
